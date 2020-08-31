2559aac682cd90675640c5dbc6852fb5bc1810b8
root <amos@amoscloud.com>
2020-08-31 10:07:58 +0800

    first commit

---
+++ amos/src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Amos
+ * E-mail: amos@amoscloud.com
+ * Date: 2020/8/31
+ * Time: 9:35
+ * Description:
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("    sadasd");
+    }
+}

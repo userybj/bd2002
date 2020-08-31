2a32a3f326cd9dbd026b7745ac54151eb525c8b0
信沫沉鱼 <wrx_5764@163.com>
2020-08-31 11:14:44 +0800

    Merge remote-tracking branch 'origin/master' into master

---
+++ supreme/src/main/java/com/wrx/Test.java
@@ -0,0 +1,16 @@
+package com.wrx;
+
+/**
+ * @author 19425
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        String[] name ={"孙尚香","刘备","孙权","张飞"};
+        for (String names:name) {
+            System.out.println(names.toString());
+        }
+    }
+
+
+}
